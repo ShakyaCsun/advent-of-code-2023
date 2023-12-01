4a4516c2f4ad6bc5008a57c2bcf103a793faf5fd
Srishan Shakya <csunshakya@gmail.com>
2023-12-02 00:48:01 +0545

    dart: day 1 part 2 solution

--- Dart/solutions/day01.dart
+++ Dart/solutions/day01.dart
@@ -10,7 +10,7 @@ class Day01 extends GenericDay {
   int solvePart1() {
     var solution = 0;
     for (final element in input.getPerLine()) {
-      final matches = RegExp(r'(\d)').allMatches(element);
+      final matches = RegExp(r'\d').allMatches(element);
       solution += int.parse('${matches.first[0]}${matches.last[0]}');
     }
     return solution;
@@ -18,6 +18,31 @@ class Day01 extends GenericDay {
 
   @override
   int solvePart2() {
-    return 0;
+    var solution = 0;
+    String applyCorrection(String word) {
+      const corrections = {
+        'one': 'o1e',
+        'two': 't2o',
+        'three': 't3e',
+        'four': 'f4r',
+        'five': 'f5e',
+        'six': 's6x',
+        'seven': 's7n',
+        'eight': 'e8t',
+        'nine': 'n9e',
+      };
+      var correctWord = word;
+      for (final MapEntry(:String key, :String value) in corrections.entries) {
+        correctWord = correctWord.replaceAll(key, value);
+      }
+      return correctWord;
+    }
+
+    for (final element in input.getPerLine()) {
+      final correctWord = applyCorrection(element);
+      final matches = RegExp(r'\d').allMatches(correctWord);
+      solution += int.parse('${matches.first[0]}${matches.last[0]}');
+    }
+    return solution;
   }
 }
