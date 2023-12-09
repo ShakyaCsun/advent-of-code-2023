de86c140c08371edc18fd57dccaa8c2cb9288411
Srishan Shakya <csunshakya@gmail.com>
2023-12-09 17:03:30 +0545

    dart: day 9 part 1 solution

---
+++ Dart/solutions/day09.dart
@@ -0,0 +1,41 @@
+import '../utils/index.dart';
+
+class Day09 extends GenericDay {
+  Day09() : super(9);
+
+  @override
+  List<List<int>> parseInput() {
+    return input
+        .getPerLine()
+        .map<List<int>>((e) => e.split(' ').map(int.parse).toList())
+        .toList();
+  }
+
+  late final parsedInput = parseInput();
+
+  @override
+  int solvePart1() {
+    var result = 0;
+    for (final numbers in parsedInput) {
+      result += getNextNumber(numbers);
+    }
+    return result;
+  }
+
+  @override
+  int solvePart2() {
+    return 0;
+  }
+
+  int getNextNumber(List<int> numbers) {
+    if (numbers.every((element) => element == 0)) {
+      return 0;
+    }
+    final length = numbers.length;
+    final differences = <int>[];
+    for (var i = 0; i < length - 1; i++) {
+      differences.add(numbers[i + 1] - numbers[i]);
+    }
+    return numbers[length - 1] + getNextNumber(differences);
+  }
+}
