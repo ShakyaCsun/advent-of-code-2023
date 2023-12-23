bbc2192bc04a24d9f93264cf731eb3e78a1e1050
Srishan Shakya <csunshakya@gmail.com>
2023-12-23 21:06:10 +0545

    dart: day 23 part 2 solution

--- Dart/solutions/day23.dart
+++ Dart/solutions/day23.dart
@@ -15,7 +15,9 @@ class Day23 extends GenericDay {
 
   @override
   int solvePart2() {
-    return 0;
+    final field = parseInput();
+
+    return field.findAllPaths();
   }
 }
 
@@ -30,6 +32,122 @@ extension PathFinder on Field<String> {
       currentPosition = cameFrom[currentPosition]!;
       yield currentPosition;
     }
+  }
+
+  Iterable<Position> validNeighbours(Position current) {
+    return adjacent(current.x, current.y)
+        .whereNot((element) => getValueAtPosition(element) == '#')
+        .toSet();
+  }
+
+  bool isDecisionNode(Position position) {
+    return validNeighbours(position).toList().length > 2;
+  }
+
+  ({Position next, int cost}) getStepsToNextDecisionNode(
+    Position start,
+    Position going,
+  ) {
+    var previous = start;
+    var current = going;
+    var steps = 1;
+
+    while (true) {
+      final next = validNeighbours(current)
+          .where((element) => element != previous)
+          .toSet();
+      if (next.length == 1) {
+        steps++;
+        previous = current;
+        current = next.first;
+      } else {
+        return (next: current, cost: steps);
+      }
+    }
+  }
+
+  int findAllPaths() {
+    final start = firstWhere('.');
+    final goal = (
+      field[height - 1].lastIndexWhere((element) => element == '.'),
+      height - 1,
+    );
+    final decisionNodes = <Position>{};
+    forEach((p0, p1) {
+      if (getValueAt(p0, p1) != '#' && isDecisionNode((p0, p1))) {
+        decisionNodes.add((p0, p1));
+      }
+    });
+    final nodeDestinations =
+        <Position, Set<({Position destination, int cost})>>{};
+    var firstDecisionNode = start;
+    var costToFirstDecisionNode = 0;
+    for (final decisionNode in decisionNodes) {
+      for (final neighbour in validNeighbours(decisionNode)) {
+        final (:next, :cost) =
+            getStepsToNextDecisionNode(decisionNode, neighbour);
+        if (next == start) {
+          costToFirstDecisionNode = cost;
+          firstDecisionNode = decisionNode;
+          continue;
+        }
+        nodeDestinations[decisionNode] = {
+          ...nodeDestinations[decisionNode] ?? {},
+          (destination: next, cost: cost),
+        };
+      }
+    }
+    // final queue = <(Position, Set<Position>, int)>[
+    //   (firstDecisionNode, {start, firstDecisionNode}, costToFirstDecisionNode),
+    // ];
+    var maxCost = 0;
+    var pathCount = 0;
+    final seen = nodeDestinations.map(
+      (key, value) => MapEntry(key, false),
+    );
+    void dfs(Position vertex, int currentCost) {
+      if (seen[vertex] ?? false) {
+        return;
+      }
+      seen[vertex] = true;
+      if (vertex == goal) {
+        pathCount++;
+        if (currentCost > maxCost) {
+          maxCost = currentCost;
+        }
+        seen[vertex] = false;
+        return;
+      }
+      for (final (:destination, :cost) in nodeDestinations[vertex]!) {
+        dfs(destination, currentCost + cost);
+      }
+      seen[vertex] = false;
+    }
+
+    dfs(firstDecisionNode, costToFirstDecisionNode);
+    // while (queue.isNotEmpty) {
+    //   final (current, path, currentCost) = queue.removeAt(0);
+    //   if (current == goal) {
+    //     pathCount++;
+    //     if (currentCost > maxCost) {
+    //       maxCost = currentCost;
+    //     }
+    //     if (pathCount % 10000 == 0) {
+    //       print(path);
+    //       print('$pathCount) $currentCost - $maxCost');
+    //     }
+    //     continue;
+    //   }
+    //   for (final (:destination, :cost) in nodeDestinations[current]!) {
+    //     final newPath = {...path};
+    //     if (newPath.add(destination)) {
+    //       queue.add((destination, newPath, currentCost + cost));
+    //     }
+    //   }
+    // }
+    // print(pathCount);
+
+    return maxCost;
   }
 
   int findLongestTrail() {
@@ -95,6 +213,12 @@ extension PathFinder on Field<String> {
       }
     }
     return retracePath(cameFrom, goal).length - 1;
+  }
+}
+
+extension on ({Position from, Position to}) {
+  ({Position from, Position to}) get reverse {
+    return (from: to, to: from);
   }
 }
 
--- Dart/test/day23_test.dart
+++ Dart/test/day23_test.dart
@@ -62,7 +62,7 @@ const _puzzleSolutionPart1 = 2166;
 /// This can only be filled out after you have solved the puzzle and is used
 /// for regression testing when refactoring.
 /// As long as the variable is `null`, the tests will be skipped.
-const _puzzleSolutionPart2 = null;
+const _puzzleSolutionPart2 = 6378;
 
 void main() {
   group(
